dce6a5a3b3a022ed01f9e770115113aacf6ff188
Arthur Foelsche <arthurf@27259.no-reply.drupal.org>
2009-01-09 22:38:43 +0000

    first commit of the new code into the media module

---
+++ media.js
@@ -0,0 +1,15 @@
+// $Id$
+
+/**
+ * @file 
+ * This file handles the JS for Media Module functions
+ */
+ 
+ 
+ $(document).ready( function () { 
+   // get the list of form elements that need to be hidden
+ 
+
+ 
+ });
+ 
