a6829b5f898e7877eb97091811af98a903aabcca
Maarten Verbaarschot <mverbaar@389188.no-reply.drupal.org>
2009-10-25 03:01:38 +0000

    Add 'select all/none' links to tthumbnails display. (Needs more work.)

--- media.js
+++ media.js
@@ -2,82 +2,39 @@
 
 /**
  * @file
- * This file handles the JS for Media Module functions
+ * Handles the JS for Media module functions.
  */
 
 (function ($) {
 
 /**
-* This handles the activation of drawers on the media browser form
-* @TODO make this a drupal behavior
-*/
-$(document).ready( function () {
-  // hide the display information on page load
-  $('.media.browser.display').each(function() {
-    $(this).hide();
-  });
-
-  // Activate drawers when the page loads
-  $('.media.browser .ui-tabs-panel .drawers .item-list li.first').addClass('active');
-
-  // Activate displays when the page loads
-  $('.media.browser .ui-tabs-panel .display.first').addClass('active').show();
-
-  // now we need to bind click functionality on drawers to display
-  $('.media.browser .ui-tabs-panel .drawers .item-list ul li, .drawers li a').bind('click', function () {
-    // get the href id that we want to display
-    // is this an a element?
-    var display_id = $(this).attr('href');
-
-    if (display_id == undefined) {
-      var display_id = $(this).children('a').attr('href');
-    }
-
-    // we need to get the container that this drawer is in
-    var parent = $(this).parents('.ui-tabs-panel').attr('id');
-
-    // hide current active display
-    $('#'+parent+' .display.active').removeClass('active').hide();
-
-    // set any drawers to not active
-    $('#'+parent+' .drawers li.active').removeClass('active');
-
-    // make this drawer active
-    $(this).addClass('active');
-
-    // make the correct display active
-    $(display_id).addClass('active').show();
-  });
-});
-
-/**
-* we need to hide any form elements that were replaced by the media browser form
-* activate the add button, and hide the browser
-*/
-$(document).ready(function () {
-  $('.media.replace').hide();
-  
-  $('.media.browser.activation').each(function () {
-    $(this).next('.media.browser').hide();
-    
-    $(this).click(function () {
-      $(this).next('.media.browser').slideDown('slow');
-      $(this).slideUp();
-    });
-  });
-});
-
-/**
  * Thumbnails display
  */
 $(document).ready(function () {
+  // Implements 'select all/none'.
+  $('<div class="media-thumbnails-select" />')
+    .append('<strong>' + Drupal.t('Select') + ':</strong> <a href="#">' + Drupal.t('all') + '</a>, <a href="#">' + Drupal.t('none') + '</a>')
+    .prependTo('.media-display-thumbnails')
+    .find('a')
+    .click(function () {
+      var link = $(this);
+      switch (link.text()) {
+        case Drupal.t('all'):
+          link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', true);
+          break;
+        case Drupal.t('none'):
+          link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', false);
+          break;
+      }
+      return false;
+    });
+    
+  // Add an extra class to selected thumbnails.
   $('div.media-thumbnail :checkbox', '.media-display-thumbnails').each(function () {
     var checkbox = $(this);
-    
     if (checkbox.is(':checked')) {
       checkbox.parents('div.media-thumbnail').addClass('media-thumbnail-checked');
     }
-    
     checkbox.change(function () {
       if (checkbox.is(':checked')) {
         checkbox.parents('div.media-thumbnail').addClass('media-thumbnail-checked');
