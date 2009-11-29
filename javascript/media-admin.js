1604209a07bed4852d0309b3c11d6ecb5c30f1eb
Jody Hamilton <jhamiltoorion@gmail.com>
2009-11-29 21:57:06 +0000

    Adjust media-admin.js to D7 standards

--- javascript/media-admin.js
+++ javascript/media-admin.js
@@ -10,40 +10,42 @@
 /**
  * Thumbnails display
  */
-$(document).ready(function () {
-  // Implements 'select all/none'.
-  $('<div class="media-thumbnails-select" />')
-    .append('<strong>' + Drupal.t('Select') + ':</strong> <a href="#">' + Drupal.t('all') + '</a>, <a href="#">' + Drupal.t('none') + '</a>')
-    .prependTo('.media-display-thumbnails')
-    .find('a')
-    .click(function () {
-      var link = $(this);
-      switch (link.text()) {
-        case Drupal.t('all'):
-          link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', true);
-          break;
-        case Drupal.t('none'):
-          link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', false);
-          break;
-      }
-      return false;
-    });
-    
-  // Add an extra class to selected thumbnails.
-  $('div.media-thumbnail :checkbox', '.media-display-thumbnails').each(function () {
-    var checkbox = $(this);
-    if (checkbox.is(':checked')) {
-      checkbox.parents('div.media-thumbnail').addClass('media-thumbnail-checked');
-    }
-    checkbox.change(function () {
+Drupal.behaviors.mediaAdmin = {
+  attach: function (context) {
+    // Implements 'select all/none'.
+    $('<div class="media-thumbnails-select" />')
+      .append('<strong>' + Drupal.t('Select') + ':</strong> <a href="#">' + Drupal.t('all') + '</a>, <a href="#">' + Drupal.t('none') + '</a>')
+      .prependTo('.media-display-thumbnails')
+      .find('a')
+      .click(function () {
+        var link = $(this);
+        switch ($(this).text()) {
+          case Drupal.t('all'):
+            link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', true);
+            break;
+          case Drupal.t('none'):
+            link.parents('.media-display-thumbnails').find('div.media-thumbnail :checkbox').attr('checked', false);
+            break;
+        }
+        return false;
+      });
+
+    // Add an extra class to selected thumbnails.
+    $('div.media-thumbnail :checkbox', '.media-display-thumbnails').each(function () {
+      var checkbox = $(this);
       if (checkbox.is(':checked')) {
         checkbox.parents('div.media-thumbnail').addClass('media-thumbnail-checked');
       }
-      else {
-        checkbox.parents('div.media-thumbnail-checked').removeClass('media-thumbnail-checked');
-      }
-    });
-  });
-});
-
+      checkbox.change(function () {
+        if (checkbox.is(':checked')) {
+          checkbox.parents('div.media-thumbnail').addClass('media-thumbnail-checked');
+        }
+        else {
+          checkbox.parents('div.media-thumbnail-checked').removeClass('media-thumbnail-checked');
+        }
+      });
+    }); 
+  }
+};
+  
 })(jQuery);
