ffe7757428666ac3f0ae78db4094b96d85960a00
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-01-07 23:22:31 +0000

    Made improvements to wysiwyg integration.  It's currently broken, but working w/ sun to figure out how to build the tagmap / attach stuff

--- javascript/wysiwyg-media.js
+++ javascript/wysiwyg-media.js
@@ -17,16 +17,24 @@ Drupal.wysiwyg.plugins.media = {
   },
   
   /**
-   * Atach function, called when a rich text editor loads
+   * Atach function, called when a rich text editor loads.
+   * This finds all [[tags]] and replaces them with the html
+   * that needs to show in the editor.
+   * 
    */
   attach: function(content, settings, instanceId) {
+    debug.debug(instanceId);
+    debug.debug(settings);
 	  matches = content.match(/\[\[.*?\]\]/g);
 	  tagmap = Drupal.settings.tagmap;
 	  if(matches) {
 	  for (i=0; i< matches.length; i++) {
 		  for (var tagContent in tagmap ) {
 			  if (tagContent === matches[i]) {
-				content = content.replace(matches[i],tagmap[tagContent]);
+			    // This probably needs some work...
+			    // We need to somehow get the fid propogated here.
+			    // We really want to 
+			    content = content.replace(matches[i],tagmap[tagContent]);
 			  }
 		  }
 	  }
@@ -39,11 +47,12 @@ Drupal.wysiwyg.plugins.media = {
    */
   detach: function(content, settings, instanceId) {
     var $content = jQuery('<div>' + content + '</div>');
-    jQuery('a.media-thumbnail',$content).each(function (elem){
+    jQuery('div.media-preview',$content).each(function (elem){
       var imgNode = jQuery("img",this);
     	var $foo = {
     		"type": 'media',
-    		"view_mode": 'thumbnail',
+    		//@todo: This will be selected from the format form
+    		"view_mode": 'original',
     		"fid" : imgNode.attr('fid'),
     		"attributes": { 
     			"width" : imgNode.attr('width'),
