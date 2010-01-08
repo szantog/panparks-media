c45055ec0d2de16de36550dc78f81eaef5b02415
Dipen Chaudhary <dipen_chaudhary@100291.no-reply.drupal.org>
2010-01-08 23:49:49 +0000

    Fixed the fid to be picked from mediaObject in JS in invoke and attach

--- javascript/wysiwyg-media.js
+++ javascript/wysiwyg-media.js
@@ -7,13 +7,12 @@ Drupal.wysiwyg.plugins.media = {
     if (data.format == 'html') {
       options = {};
       jQuery().mediaBrowser( function (mediaFiles, options) {
-    	  // Currently we can select only one file from the browser
-    	  mediaFile = mediaFiles[0];
-    	  // Insert the preview of the file returned into the editor.
-    	  Drupal.wysiwyg.instances[instanceId].insert(mediaFile.preview);
-      	},options);
-    }
-    
+    	// Insert the preview of the file returned into the editor.
+        preview = jQuery(mediaFiles[0].preview);
+    	jQuery('img',preview).attr('fid',mediaFiles[0].fid);
+    	Drupal.wysiwyg.instances[instanceId].insert(Drupal.wysiwyg.plugins.media.addWrapper(preview.html()));
+      },options);
+    }    
   },
   
   /**
@@ -23,21 +22,24 @@ Drupal.wysiwyg.plugins.media = {
    * 
    */
   attach: function(content, settings, instanceId) {
-    debug.debug(instanceId);
-    debug.debug(settings);
-	  matches = content.match(/\[\[.*?\]\]/g);
-	  tagmap = Drupal.settings.tagmap;
+    matches = content.match(/\[\[.*?\]\]/g);
+	tagmap = Drupal.settings.tagmap;
 	  if(matches) {
-	  for (i=0; i< matches.length; i++) {
+	    for (i=0; i< matches.length; i++) {
 		  for (var tagContent in tagmap ) {
-			  if (tagContent === matches[i]) {
-			    // This probably needs some work...
-			    // We need to somehow get the fid propogated here.
-			    // We really want to 
-			    content = content.replace(matches[i],tagmap[tagContent]);
-			  }
+		    if (tagContent === matches[i]) {
+			  // This probably needs some work...
+			  // We need to somehow get the fid propogated here.
+			  // We really want to
+			  matches[i] = matches[i].replace('[[','');
+			  matches[i] = matches[i].replace(']]','');
+			  mediaObj = JSON.parse(matches[i]);
+			  imgMarkup = jQuery(tagmap[tagContent]);
+			  jQuery('img',imgMarkup).attr('fid',mediaObj.fid);
+			  content = content.replace(tagContent,this.addWrapper(imgMarkup.html()));
+			}
 		  }
-	  }
+	    }
 	  }
 	  return content;
   },
@@ -47,22 +49,25 @@ Drupal.wysiwyg.plugins.media = {
    */
   detach: function(content, settings, instanceId) {
     var $content = jQuery('<div>' + content + '</div>');
-    jQuery('div.media-preview',$content).each(function (elem){
+    jQuery('div.media-embedded',$content).each(function (elem){
       var imgNode = jQuery("img",this);
-    	var $foo = {
-    		"type": 'media',
-    		//@todo: This will be selected from the format form
-    		"view_mode": 'original',
-    		"fid" : imgNode.attr('fid'),
-    		"attributes": { 
-    			"width" : imgNode.attr('width'),
-    			"height" : imgNode.attr('height'),
-    	  }
+      tagContent = {
+        "type": 'media',
+    	//@todo: This will be selected from the format form
+    	"view_mode": 'media_original',
+    	"fid" : imgNode.attr('fid'),
+    	"attributes": {
+    	  "width" : imgNode.attr('width'),
+    	  "height" : imgNode.attr('height'),
     	}
-    	debug.debug(jQuery("img.media-img-thumbnail").attr('width'));
-    	$foo = '[[' + JSON.stringify($foo) + ']]';
-    	jQuery(this).replaceWith($foo);
+      }
+      tagContent = '[[' + JSON.stringify(tagContent) + ']]';
+      jQuery(this).replaceWith(tagContent);
     });
     return $content.html();
   },
+  
+  addWrapper: function(htmlContent) {
+	  return '<div class="media-embedded">' + htmlContent + '</div>';  
+  },  
 };
