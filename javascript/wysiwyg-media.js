7d5e29425dffc4e173d3cb1789a24a4659b848a5
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-01-15 17:35:57 +0000

    JavaScript cleanup

--- javascript/wysiwyg-media.js
+++ javascript/wysiwyg-media.js
@@ -1,76 +1,84 @@
-Drupal.wysiwyg.plugins.media = {
-
-  /**
-   * Execute the button.
-   */
-  invoke: function(data, settings, instanceId) {
-    if (data.format == 'html') {
-      options = {};
-      $().mediaBrowser( function (mediaFiles, options) {
-    	// Insert the preview of the file returned into the editor.
-        preview = $(mediaFiles[0].preview);
-    	$('img',preview).attr('fid',mediaFiles[0].fid);
-    	Drupal.wysiwyg.instances[instanceId].insert(Drupal.wysiwyg.plugins.media.addWrapper(preview.html()));
-      },options);
-    }    
-  },
+(function ($) {
   
-  /**
-   * Atach function, called when a rich text editor loads.
-   * This finds all [[tags]] and replaces them with the html
-   * that needs to show in the editor.
-   * 
-   */
-  attach: function(content, settings, instanceId) {  
-	//@TODO Lost $ reference for some reason :(, restoring it with jQuery obj
-	$ = jQuery;  
-    matches = content.match(/\[\[.*?\]\]/g);
-	tagmap = Drupal.settings.tagmap;
-	console.debug(tagmap);
-	  if(matches) {
-	    for (i=0; i< matches.length; i++) {
-		  for (var tagContent in tagmap ) {
-		    if (tagContent === matches[i]) {
-			  // This probably needs some work...
-			  // We need to somehow get the fid propogated here.
-			  // We really want to
-			  matches[i] = matches[i].replace('[[','');
-			  matches[i] = matches[i].replace(']]','');
-			  mediaObj = JSON.parse(matches[i]);
-			  imgMarkup = $(tagmap[tagContent]);
-			  $('img',imgMarkup).attr('fid',mediaObj.fid);
-			  content = content.replace(tagContent,this.addWrapper(imgMarkup.html()));
-			}
-		  }
-	    }
-	  }
-	  return content;
-  },
-
-  /**
-   * Detach function, called when a rich text editor detaches
-   */
-  detach: function(content, settings, instanceId) {
-    var content = $('<div>' + content + '</div>');
-    $('div.media-embedded',content).each(function (elem){
-      var imgNode = $("img",this);
-      tagContent = {
-        "type": 'media',
-    	//@todo: This will be selected from the format form
-    	"view_mode": 'media_original',
-    	"fid" : imgNode.attr('fid'),
-    	"attributes": {
-    	  "width" : imgNode.attr('width'),
-    	  "height" : imgNode.attr('height'),
-    	}
-      }
-      tagContent = '[[' + JSON.stringify(tagContent) + ']]';
-      $(this).replaceWith(tagContent);
-    });
-    return content.html();
-  },
+  Drupal.wysiwyg.plugins.media = {
+    /**
+     * Execute the button.
+     */
+    invoke: function(data, settings, instanceId) {
+      if (data.format == 'html') {
+        options = {};
+        $().mediaBrowser( function (mediaFiles, options) {
+      	// Insert the preview of the file returned into the editor.
+          preview = $(mediaFiles[0].preview);
+          $('img',preview).attr('fid',mediaFiles[0].fid);
+          Drupal.wysiwyg.instances[instanceId].insert(Drupal.wysiwyg.plugins.media.addWrapper(preview.html()));
+        },options);
+      }    
+    },
+    
+    /**
+     * Atach function, called when a rich text editor loads.
+     * This finds all [[tags]] and replaces them with the html
+     * that needs to show in the editor.
+     * 
+     */
+    attach: function(content, settings, instanceId) {  
+  	//@TODO Lost $ reference for some reason :(, restoring it with jQuery obj
+  	$ = jQuery;  
+      matches = content.match(/\[\[.*?\]\]/g);
+  	tagmap = Drupal.settings.tagmap;
+  	console.debug(tagmap);
+  	  if(matches) {
+  	    for (i=0; i< matches.length; i++) {
+  		  for (var tagContent in tagmap ) {
+  		    if (tagContent === matches[i]) {
+  			  // This probably needs some work...
+  			  // We need to somehow get the fid propogated here.
+  			  // We really want to
+  			  matches[i] = matches[i].replace('[[','');
+  			  matches[i] = matches[i].replace(']]','');
+  			  mediaObj = JSON.parse(matches[i]);
+  			  imgMarkup = $(tagmap[tagContent]);
+  			  $('img',imgMarkup).attr('fid',mediaObj.fid);
+  			  content = content.replace(tagContent,this.addWrapper(imgMarkup.html()));
+  			}
+  		  }
+  	    }
+  	  }
+  	  return content;
+    },
   
-  addWrapper: function(htmlContent) {
-    return '<div class="media-embedded">' + htmlContent + '</div>';  
-  },  
-};
+    /**
+     * Detach function, called when a rich text editor detaches
+     */
+    detach: function(content, settings, instanceId) {
+      var content = $('<div>' + content + '</div>');
+      $('div.media-embedded',content).each(function (elem){
+        var imgNode = $("img",this);
+        tagContent = {
+          "type": 'media',
+        	//@todo: This will be selected from the format form
+        	"view_mode": 'media_original',
+        	"fid" : imgNode.attr('fid'),
+        	"attributes": {
+        	  "width" : imgNode.attr('width'),
+        	  "height" : imgNode.attr('height')
+        	}
+        };
+        tagContent = '[[' + JSON.stringify(tagContent) + ']]';
+        $(this).replaceWith(tagContent);
+      });
+      return content.html();
+    },
+    
+    addWrapper: function(htmlContent) {
+      return '<div class="media-embedded">' + htmlContent + '</div>';  
+    }
+  };
+  
+  Drupal.media = Drupal.media || {};
+  Drupal.media.formatForm = {
+    
+  }
+
+})(jQuery);
