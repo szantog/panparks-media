5f9ba7c08b90d4a569d427a87a6645c2712e3d5f
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-01-17 04:29:12 +0000

    Added a preliminary from url tab and form

---
+++ javascript/plugins/media.fromurl.js
@@ -0,0 +1,24 @@
+(function ($) {
+  namespace('Drupal.media.browser.plugin');
+  
+  Drupal.media.browser.plugin.fromurl = function(mediaBrowser, options) {
+    return {
+      /* Abstract */
+      init: function() {
+        tabset = mediaBrowser.getTabset();
+        tabset.tabs('add', '#fromurl', 'From URL');
+        mediaBrowser.listen('tabs.tabSelected', function (e, id) {
+          if (id == 'fromurl') {
+            mediaBrowser.getContentArea().load(Drupal.settings.basePath + 'admin/content/media/add/from_url?destination=' + window.location.href +' form');
+          }
+        });
+      }
+    }
+  };
+  
+  // For now, I guess self registration makes sense.
+  // Really though, we should be doing it via drupal_add_js and some settings
+  // from the drupal variable.
+  //@todo: needs a review.
+  Drupal.media.browser.register('fromurl', Drupal.media.browser.plugin.fromurl, {});
+})(jQuery);
