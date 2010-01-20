0aa869f581b34d3b5d59fb8a6f5411674005b104
aaronwinborn <aaron@aaronwinborn.com>
2010-01-20 19:21:59 +0000

    documentation

--- javascript/media-format-form.js
+++ javascript/media-format-form.js
@@ -1,17 +1,32 @@
+// $Id$
+
+/**
+ *  @file
+ *  Attach behaviors to formatter radio select when selecting a media's display
+ *  formatter.
+ */
+
+// Set the namespace.
 namespace('Drupal.media');
+
 Drupal.media.mediaFormatSelected = {};
+
 (function ($) {
 Drupal.behaviors.mediaFormatForm = {
   attach: function (context, settings) {
-    //Setup the value to start with.
-    
+    // When we select a new radio, then load it up to be sent to the WYSIWYG.
     $("input[name='format']", context).change(Drupal.media.mediaFormatSelected);
+
+    //Setup the value to start with.
     $("input[name='format']:checked").trigger('change');
   }
 };
 
 Drupal.media.mediaFormatSelected = function (e) {
+  // What's the display formatter we've selected?
   var formatType = $(this).val();
+
+  // Set the HTML to be sent to the WYSIWYG browser.
   var formattedMedia = {type: formatType, html: Drupal.settings.media.formatFormFormats[formatType]};
   debug.debug(formattedMedia);
   Drupal.media.formattedMedia = formattedMedia;
