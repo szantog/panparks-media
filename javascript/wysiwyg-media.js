8849158e1bc5e83b18a095faf51c9b23ff8607de
aaronwinborn <aaron@aaronwinborn.com>
2010-02-16 02:12:53 +0000

    by aaron: documentation

--- javascript/wysiwyg-media.js
+++ javascript/wysiwyg-media.js
@@ -1,21 +1,33 @@
+// $Id$
+
+/**
+ *  @file
+ *  Attach Media WYSIWYG behaviors.
+ */
+
+// Define the namespace.
 Drupal.media = Drupal.media || {};
 
+// jQuery safe call.
 (function ($) {
-  
+
+  // Define the behavior.
   Drupal.wysiwyg.plugins.media = {
     /**
      * Execute the button.
+     * @TODO: Debug calls from this are never called. What's its function?
      */
     invoke: function (data, settings, instanceId) {
       if (data.format == 'html') {
         Drupal.media.popups.mediaBrowser(function (mediaFiles) {
           Drupal.wysiwyg.plugins.media.mediaBrowserOnSelect(mediaFiles, instanceId);
         });
-      }    
-    },    
+      }
+    },
 
     /**
      * Respond to the mediaBrowser's onSelect event.
+     * @TODO: Debug calls from this are never called. What's its function?
      */
     mediaBrowserOnSelect: function (mediaFiles, instanceId) {
       var mediaFile = mediaFiles[0];
@@ -24,12 +36,12 @@ Drupal.media = Drupal.media || {};
         debug.debug(formattedMedia);
         Drupal.wysiwyg.plugins.media.insertMediaFile(mediaFile, formattedMedia.type, formattedMedia.html, formattedMedia.options, Drupal.wysiwyg.instances[instanceId]);
       }, options);
-      
+
       return;
     },
 
     insertMediaFile: function(mediaFile, viewMode, formattedMedia, options, wysiwygInstance) {
-      
+
       if(typeof Drupal.settings.tagmap == 'undefined') {
         Drupal.settings.tagmap = { };
       }
@@ -41,7 +53,7 @@ Drupal.media = Drupal.media || {};
       //
       var imgElement = $(this.stripDivs(formattedMedia));
       this.addImageAttributes(imgElement, mediaFile.fid, viewMode, options);
-      
+
       var toInsert = this.outerHTML(imgElement);
       // Create an inline tag
       var inlineTag = Drupal.wysiwyg.plugins.media.createTag(imgElement);
@@ -49,16 +61,16 @@ Drupal.media = Drupal.media || {};
       Drupal.settings.tagmap[inlineTag] = toInsert;
       wysiwygInstance.insert(toInsert);
     },
-    
+
     /**
      * Gets the HTML content of an element
-     * 
+     *
      * @param jQuery element
      */
     outerHTML: function(element) {
       return $('<div>').append( element.eq(0).clone() ).html();
     },
-    
+
     addImageAttributes: function(imgElement, fid, view_mode, additional) {
       imgElement.attr('fid', fid);
       imgElement.attr('view_mode', view_mode);
@@ -70,29 +82,29 @@ Drupal.media = Drupal.media || {};
       // Class so we can find this image later.
       imgElement.addClass('media-image');
     },
-    
+
     /**
      * Due to problems handling wrapping divs in ckeditor, this is needed.
-     * 
+     *
      * Going forward, if we don't care about supporting other editors
      * we can use the fakeobjects plugin to ckeditor to provide cleaner
      * transparency between what Drupal will output <div class="field..."><img></div>
      * instead of just <img>, for now though, we're going to remove all the stuff surrounding the images.
-     * 
+     *
      * @param String formattedMedia
      *  Element containing the image
-     *  
+     *
      * @return HTML of <img> tag inside formattedMedia
      */
     stripDivs: function(formattedMedia) {
       return $('<div>').append( $('img', $(formattedMedia)).eq(0).clone() ).html();
     },
-    
+
     /**
-     * Atach function, called when a rich text editor loads.
+     * Attach function, called when a rich text editor loads.
      * This finds all [[tags]] and replaces them with the html
      * that needs to show in the editor.
-     * 
+     *
      */
     attach: function(content, settings, instanceId) {
       var matches = content.match(/\[\[.*?\]\]/g);
@@ -107,12 +119,12 @@ Drupal.media = Drupal.media || {};
             // We really want to
             var tagContent = tagmap[inlineTag];
             var mediaMarkup = this.stripDivs(tagContent); // THis is <div>..<img>
-            
+
             var _tag = inlineTag;
             _tag = _tag.replace('[[','');
             _tag = _tag.replace(']]','');
             mediaObj = JSON.parse(_tag);
-            
+
             var imgElement = $(mediaMarkup);
             this.addImageAttributes(imgElement, mediaObj.fid, mediaObj.view_mode);
             var toInsert = this.outerHTML(imgElement);
@@ -124,7 +136,7 @@ Drupal.media = Drupal.media || {};
       }
       return content;
     },
-  
+
     /**
      * Detach function, called when a rich text editor detaches
      */
@@ -136,7 +148,7 @@ Drupal.media = Drupal.media || {};
       });
       return content.html();
     },
-    
+
     /**
      * @param jQuery imgNode
      *  Image node to create tag from
@@ -145,9 +157,9 @@ Drupal.media = Drupal.media || {};
       // Currently this is the <img> itself
       // Collect all attribs to be stashed into tagContent
       var mediaAttributes = {};
-      
+
       var imgElement = imgNode[0];
-      
+
       //@todo: this does not work in IE, width and height are always 0.
       for (i=0; i< imgElement.attributes.length; i++) {
         var attr = imgElement.attributes[i];
