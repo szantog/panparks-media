553d3d6240267633ec9efaa5ca617c09a5531f88
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-03-14 10:28:06 +0000

    #740546 and #741964 Big improvements to media multi-value field, media_gallery module nearing alpha, CRUD is more or less complete.  Operation: Mediacrity at 50%

--- javascript/media.fields.js
+++ javascript/media.fields.js
@@ -7,41 +7,40 @@ Drupal.behaviors.mediaBrowserFields ={
   attach: function (context, settings) {
     // For each media field
     $('.field-type-media', context).once('mediaBrowserLaunch', function () {
-      //options = Drupal.settings.media.fields[this.id];
-      
-      //debug.debug(this.id);
-      //debug.debug(options);
+      var options = Drupal.settings.media.fields[this.id];
+      // For each widget
+      $('.media-widget', this).once('mediaBrowserLaunch', function () {
 
-      var fidField = $('.fid', $(this));
-      var previewField = $('.preview', $(this));
-      
-      if (Drupal.settings.media.debug) {
-        // @TODO: Remove this, here for debugging purposes.
-        fidField.show();
-        var debugField = $('.file_info', this);
-      }
-      
-      // When someone clicks the link to pick media:
-      $('.launcher', this).bind('click', function () {
-        var options = {};
-        // Launch the browser, providing the following callback function
-        // @TODO: This should not be an anomyous function.
-        Drupal.media.popups.mediaBrowser(function (mediaFiles, options) {
-          if (mediaFiles.length < 0) {
-            return;
-          }
-          var mediaFile = mediaFiles[0];
-          // Set the value of the filefield fid (hidden).
-          fidField.val(mediaFile.fid);
-          // Set the preview field HTML
-          debug.debug(mediaFile.preview);
-          previewField.html(mediaFile.preview);
-          if (Drupal.settings.media.debug) {
-            // @TODO: Remove this, here for debugging purposes.
-            debugField.html(JSON.stringify(mediaFile));
-          }
+        //options = Drupal.settings.media.fields[this.id];
+        var fidField = $('.fid', this);
+        var previewField = $('.preview', this);
+        
+        if (Drupal.settings.media.debug) {
+          // @TODO: Remove this, here for debugging purposes.
+          fidField.show();
+          var debugField = $('.file_info', this);
+        }
+
+        // When someone clicks the link to pick media:
+        $('.launcher', this).bind('click', function () {
+          // Launch the browser, providing the following callback function
+          // @TODO: This should not be an anomyous function.
+          Drupal.media.popups.mediaBrowser(function (mediaFiles) {
+            if (mediaFiles.length < 0) {
+              return;
+            }
+            var mediaFile = mediaFiles[0];
+            // Set the value of the filefield fid (hidden).
+            fidField.val(mediaFile.fid);
+            // Set the preview field HTML
+            previewField.html(mediaFile.preview);
+            if (Drupal.settings.media.debug) {
+              // @TODO: Remove this, here for debugging purposes.
+              debugField.html(JSON.stringify(mediaFile));
+            }
+          }, options);
+          return false;
         });
-        return false;
       });
     });
   }
