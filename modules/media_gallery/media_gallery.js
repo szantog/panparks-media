4876b096875c66a162754a65e33cc2c2f0092e15
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-03-18 12:32:07 +0000

    Added hideable fieldsets for media_gallery

---
+++ modules/media_gallery/media_gallery.js
@@ -0,0 +1,20 @@
+// $Id;
+
+(function ($) {
+  Drupal.behaviors.experimentalMediaBrowser = {
+    attach: function (context) {
+      $('.media-widget-closed', context).once('mediaWidgetDrawrerDone', this.processWidget);
+    },
+
+    processWidget: function() {
+      var $this = $(this);
+      var $wrapper = $('.gallery-fields-wrapper', this);
+      $wrapper.slideUp();
+        $('.edit-description-link', this).bind('click', function() {
+          $wrapper.slideDown();
+          $this.addClass('media-widget-open');
+          $this.removeClass('media-widget-closed');
+        });
+    }
+  };
+})(jQuery);
