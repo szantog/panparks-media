5b57985caaa9695a8c4babd91c2fe2e745786631
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-07-08 02:55:30 +0000

    #846710 by efflugenstia and JacobSingh: get media as a field working again.  And move it to media_elements

--- javascript/media.js
+++ javascript/media.js
@@ -30,3 +30,56 @@ related to the subtab.
 
 
 */
+// $Id$
+
+(function ($) {
+
+/**
+ * Loads media browsers and callbacks, specifically for media as a field.
+ */
+Drupal.behaviors.mediaElement = {
+  attach: function (context, settings) {
+    // Options set from media.fields.inc for the types, etc to show in the browser.
+    
+    // For each widget (in case of multi-entry)
+    $('.media-widget', context).once('mediaBrowserLaunch', function () {
+      var options = settings.media.elements[this.id];
+      debug.debug(this);
+      debug.debug(this.id);
+      globalOptions = {};
+      if (options.global != undefined) {
+        var globalOptions = options.global;
+      }
+      //options = Drupal.settings.media.fields[this.id];
+      var fidField = $('.fid', this);
+      var previewField = $('.preview', this);
+      // When someone clicks the link to pick media (or clicks on an existing thumbnail)
+      $('.launcher', this).bind('click', function () {
+        // Launch the browser, providing the following callback function
+        // @TODO: This should not be an anomyous function.
+        Drupal.media.popups.mediaBrowser(function (mediaFiles) {
+          if (mediaFiles.length < 0) {
+            return;
+          }
+          var mediaFile = mediaFiles[0];
+          // Set the value of the filefield fid (hidden).
+          fidField.val(mediaFile.fid);
+          // Set the preview field HTML
+          previewField.html(mediaFile.preview);
+        }, globalOptions);
+        return false;
+      });
+
+      $('.media-edit-link', this).bind('click', function () {
+        var fid = fidField.val();
+        if (fid) {
+          Drupal.media.popups.mediaFieldEditor(fid, function (r) { alert(r); });
+        }
+        return false;
+      });
+
+    });
+  }
+};
+
+})(jQuery);
