054b281504a42953fe115f08b7997122f3e2f722
Jacob Singh <jacobsingh@68912.no-reply.drupal.org>
2010-11-16 05:34:08 +0000

    decrufting some of the horrid legacy JS

--- javascript/media.js
+++ javascript/media.js
@@ -5,33 +5,6 @@
  *  This file handles the JS for Media Module functions.
  */
 
-/*
-
-The naming convetion for items in this file revolves around three different
-components: tabs, subtabs and panes. Tabs are a primary navigation or grouping
-mechanism. Subtabs are related to a specific tab. Panes are the content
-related to the subtab.
-
-   ---------------------------------------
-   |                  |tab | tab2 | tab3 |
-   |                  --------------------
-   |__________ __________________________|
-   | Subtab  |                           |
-   -----------                           |
-   | Subtab2 |        Pane for           |
-   -----------      active subtab        |
-   | Subtab3 |                           |
-   -----------                           |
-   |                                     |
-   |                                     |
-   |                                     |
-   ---------------------------------------
-
-
-
-*/
-// $Id$
-
 (function ($) {
 
 /**
