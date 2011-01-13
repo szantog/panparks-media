001154738b611fc8e965f4acfb99101e374158f0
Alex Bronstein <alex@effulgentsia.net>
2011-01-13 00:27:06 +0000

    #899562 by unexpand, jackinloadup | veneti: Fixed Can not remove/unset image (and other media) assigned using multimedia asset field.

--- javascript/media.js
+++ javascript/media.js
@@ -24,6 +24,13 @@ Drupal.behaviors.mediaElement = {
       //options = Drupal.settings.media.fields[this.id];
       var fidField = $('.fid', this);
       var previewField = $('.preview', this);
+      var removeButton = $('.remove', this); // Actually a link, but looks like a button.
+
+      // Show the Remove button if there's an already selected media.
+      if (fidField.val() != 0) {
+        removeButton.show();
+      }
+
       // When someone clicks the link to pick media (or clicks on an existing thumbnail)
       $('.launcher', this).bind('click', function () {
         // Launch the browser, providing the following callback function
@@ -35,9 +42,22 @@ Drupal.behaviors.mediaElement = {
           var mediaFile = mediaFiles[0];
           // Set the value of the filefield fid (hidden).
           fidField.val(mediaFile.fid);
-          // Set the preview field HTML
+          // Set the preview field HTML.
           previewField.html(mediaFile.preview);
+          // Show the Remove button.
+          removeButton.show();
         }, globalOptions);
+        return false;
+      });
+
+      // When someone clicks the Remove button.
+      $('.remove', this).bind('click', function () {
+        // Set the value of the filefield fid (hidden).
+        fidField.val(0);
+        // Set the preview field HTML.
+        previewField.html('');
+        // Hide the Remove button.
+        removeButton.hide();
         return false;
       });
 
